8f71048aea12ab4e029f7a0da952df81c0ab92d2
Anton Kindestam <antonki@kth.se>
2018-12-19 18:22:20 +0100

    term/screen.xterm: Correct colors for TERM=screen.xterm-256color
    Quick copy paste hack from term/screen.el

---
+++ elisp/term/screen.xterm.el
@@ -0,0 +1,25 @@
+;;; screen.el --- terminal initialization for screen and tmux  -*- lexical-binding: t -*-
+;; Copyright (C) 1995, 2001-2018 Free Software Foundation, Inc.
+
+(require 'term/xterm)
+
+(defcustom xterm-screen.xterm-extra-capabilities '(modifyOtherKeys)
+  "Extra capabilities supported under \"screen\".
+Some features of screen depend on the terminal emulator in which
+it runs, which can change when the screen session is moved to another tty."
+  :version "25.1"
+  :type xterm--extra-capabilities-type
+  :group 'xterm)
+
+(defun terminal-init-screen.xterm ()
+  "Terminal initialization function for screen.xterm"
+  ;; Treat a screen terminal similar to an xterm, but don't use
+  ;; xterm-extra-capabilities's `check' setting since that doesn't seem
+  ;; to work so well (it depends too much on the surrounding terminal
+  ;; emulator, which can change during the session, bug#20356).
+  (let ((xterm-extra-capabilities xterm-screen.xterm-extra-capabilities))
+    (tty-run-terminal-initialization (selected-frame) "xterm")))
+
+(provide 'term/screen.xterm)
+
+;; screen.el ends here

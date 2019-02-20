1fbc3c75a014d185dada5f40f597b52299a8774f
Anton Kindestam <antonki@kth.se>
2019-02-20 09:35:09 +0100

    init.el: limit vterm usage to colgate only for now
    it doesn't work on ubuntu 18.10 emacs 25. and on the emacs 26 I
    installed using nix on the same ubuntu machine it works once (which
    surprised me, actually) but crashes the second time.

--- init.el
+++ init.el
@@ -84,7 +84,6 @@
  "~/.config/emacs/elisp/"
  "~/.config/emacs/vendor-elisp/"
  "~/.config/emacs/nix-mode/"
- "~/.config/emacs/emacs-libvterm/"
  "~/.elisp/")
 
 (use-package nix-mode
@@ -96,8 +95,10 @@
   :defer 1
   :config (setq etags-table-search-up-depth 99))
 
-(use-package vterm
-  :commands vterm vterm-other-window)
+(when (or i-am-colgate)
+  (maybe-add-to-load-path "~/.config/emacs/emacs-libvterm/")
+  (use-package vterm
+    :commands vterm vterm-other-window))
 
 ;; ;; load jflex-mode now (it becomes happier that way) if it is in load path
 ;; (require 'jflex-mode nil t)

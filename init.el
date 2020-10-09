7673e53d2323fb4a050f1b54620e11407a314376
Anton Kindestam <antonki@kth.se>
2020-10-09 07:09:44 +0200

    init.el: include a function that can sort words in a region
    Found on the interwebs

--- init.el
+++ init.el
@@ -716,6 +716,19 @@ Graphical browsers only."
 ;;;; END "Night mode"
 
 ;;;; BEGIN my hacky functions here
+
+;; Found here: https://www.emacswiki.org/emacs/SortWords
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+Prefixed with negative \\[universal-argument], sorts in reverse.
+
+The variable `sort-fold-case' determines whether alphabetic case
+affects the sort order.
+
+See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
+
 (defun lunix-mode ()
   (interactive)
   (c-set-style "linux")

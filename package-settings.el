cc8042313e0a98787e7bb78f0e64daf56a29f8c9
Anton Kindestam <antonki@kth.se>
2021-12-20 11:02:11 +0100

    package-settings: Switch to using unstable melpa
    stable melpa is for wusses
    
    real reason: shader-mode doesn't seem to exist in stable melpa. might
    be the case for more packages.
    
    TODO: Maybe get rid of the gnu and ELPA archives in the future. I'm
    not sure if I use any packages that are only available there any more.

--- package-settings.el
+++ package-settings.el
@@ -1,6 +1,6 @@
 (require 'package)
 
-(setq package-archives '(("melpa" . "https://stable.melpa.org/packages/")
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")
                          ("ELPA" . "https://tromey.com/elpa/")))
 (package-initialize)

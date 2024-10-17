376ac62671b10219171b29e13280fc7ed2989117
xantoz <xantoz@nowhere.com>
2024-10-17 07:27:55 +0200

    add nongnu to packages

--- package-settings.el
+++ package-settings.el
@@ -2,6 +2,7 @@
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                          ("ELPA" . "https://tromey.com/elpa/")))
 (package-initialize)
 (when (not package-archive-contents)
